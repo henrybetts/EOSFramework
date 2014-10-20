018a110c4c47ecda4dfeb1c6c52eea3a7008fa52
Henry Betts <henry@Phong-2.local>
2014-10-20 02:56:47 +0100

    Initial Commit

---
+++ EOSFramework/EOSFramework.h
@@ -0,0 +1,19 @@
+//
+//  EOSFramework.h
+//  EOSFramework
+//
+//  Created by Henry Betts on 20/10/2014.
+//  Copyright (c) 2014 Henry Betts. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for EOSFramework.
+FOUNDATION_EXPORT double EOSFrameworkVersionNumber;
+
+//! Project version string for EOSFramework.
+FOUNDATION_EXPORT const unsigned char EOSFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EOSFramework/PublicHeader.h>
+
+
---
+++ EOSFrameworkTests/EOSFrameworkTests.m
@@ -0,0 +1,40 @@
+//
+//  EOSFrameworkTests.m
+//  EOSFrameworkTests
+//
+//  Created by Henry Betts on 20/10/2014.
+//  Copyright (c) 2014 Henry Betts. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+@interface EOSFrameworkTests : XCTestCase
+
+@end
+
+@implementation EOSFrameworkTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
